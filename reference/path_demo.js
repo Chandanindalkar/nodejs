0e38e234bf46bfa34c1faba33413db2f230e8ee1
Chandanindalkar <chandan.s.indalkar@gmail.com>
2023-06-25 11:03:19 +0530

    added comments to read and recall easily

--- reference/path_demo.js
+++ reference/path_demo.js
@@ -1,14 +1,24 @@
 const path =  require('path') 
 
-// console.log(path.basename(__filename))
+// console.log(__filename) ///mnt/d/workspace/nodejs/reference/path_demo.js
 
-// console.log(path.dirname(__filename))
+// basename is the filename with extension
+// console.log(path.basename(__filename)) //path_demo.js
 
-// console.log(path.extname(__filename))
+// console.log(path.dirname(__filename)) // /mnt/d/workspace/nodejs/reference
+
+// console.log(path.extname(__filename)) // .js
 
 // console.log(path.parse(__filename))
+// {
+//   root: '/',
+//   dir: '/mnt/d/workspace/nodejs/reference',
+
+//   base: 'path_demo.js',
+//   ext: '.js',
+//   name: 'path_demo'
+// } 
 
 // join only returns a string with complete normalized path containing all the segments.
 // it does not create a dir or file
 console.log(path.join(__dirname, 'newFolder', 'test.html')) 
-
